c49a1fb51c6eaf931cde3c6c8e5a9ba4ea65948a
jbucsa <justin.bucsa@gmail.com>
2024-11-01 12:08:32 -0500

    Uploaded Spring Boot code to authorize the admin to access the dashboard

---
+++ Spring-Boot/AuthorizeAdminAccess.java
@@ -0,0 +1,7 @@
+// The following code will authorize the admin to access the dashboard.  The following code will implement Spring Security and provide the required authorization to access the dashboard.
+
+@Override 
+protected void configure(HttpSecurity http) throws Exception{
+    http.authorizeRequests()
+        antMatches("/dashboard").hasRole("ADMIN");
+}
