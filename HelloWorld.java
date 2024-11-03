48195d95c9178af20ccd4ca45eea8802de771a05
jbucsa <justin.bucsa@gmail.com>
2024-11-03 12:00:58 -0600

    Uploaded basic Hello World code.

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main (String[] args){
+        Integer a = 1;
+        Integer b =2;
+        Integer sum = a + b;
+        System.out.println("Hello Wolrd!!!! Sum = " + sum);
+    }
+}
